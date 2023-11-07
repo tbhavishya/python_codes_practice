eeed360d24a9c84eb53b61e6a669efad0274e4ef
tbhavishya <65982135+tbhavishya@users.noreply.github.com>
2023-11-07 15:05:14 -0600

    Update length_of_longest_str.py

--- length_of_longest_str.py
+++ length_of_longest_str.py
@@ -1,2 +1,11 @@
-Given a string s, find the length of the longest substring without repeating characters.
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dct = {} #initiate a empty dictionary
+        start, max_ = 0, 0 
+      for i in range(len(s)): 
+          if s[i] in dct:
+               start = max(start, dct[s[i]]+1)
+            max_ = max(max_, i-start+1)
+            dct[s[i]] = i
+          return max_
 
